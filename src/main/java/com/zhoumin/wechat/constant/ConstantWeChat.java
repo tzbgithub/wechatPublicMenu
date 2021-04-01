873c4ee8f239100f181dbff5b4f8398c05635857
tangzhangbin <564858834@qq.com>
2021-04-01 14:36:09 +0800

    初始提交

---
+++ src/main/java/com/zhoumin/wechat/constant/ConstantWeChat.java
@@ -0,0 +1,12 @@
+package com.zhoumin.wechat.constant;
+
+/**
+ * 添加id和密码信息
+ * @author zhoumin
+ * @create 2018-07-11 17:07
+ */
+public class ConstantWeChat {
+    public static final String APPID = "wx8757750483ef5899";
+
+    public static final String APPSECRET = "ac2075e29477b9877b987ec145dbcb07";
+}
